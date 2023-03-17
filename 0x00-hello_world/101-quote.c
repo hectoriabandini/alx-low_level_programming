b86693620a16bbe4daeafdeaffad04a52c27d834
hectoriabandini <abuhmoseji@gmail.com>
2023-03-16 18:51:01 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a newline, to standard error.
- * ReturnL: always 1.
+ * Return: Always 1.
  */
 
 int main(void)
