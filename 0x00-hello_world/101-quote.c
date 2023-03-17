0a0abbe00116c67293a31d310c44450e08aec18e
hectoriabandini <abuhmoseji@gmail.com>
2023-03-16 18:39:52 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - prints "and the piece of art is useful" - Dora Kopar, 2015-10-19"
+ * followed by a newline, to standard error.
+ * ReturnL: always 1.
+ */
+
+int main(void)
+
+{
+	write(2, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", 39);
+	return (1);
+}
