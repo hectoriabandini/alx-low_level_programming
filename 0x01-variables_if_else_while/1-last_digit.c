8f91a0e20531ee7836559fc1c86c23823a422f90
hectoriabandini <abuhmoseji@gmail.com>
2023-03-17 19:00:44 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,9 @@ int main(void)
 	printf("last digit of %d is %d and is 0\n", n, lastd);
 	}
 	else if (lastd < 6 && lastd != 0)
+	{
 	printf("last digit of %d is %d and is less than 6 and not 0\n", n,
 lastd);
+	}
 	return (0);
 }
