166f22cce8f837f9090be4b58bb9d4a3c7ced141
hectoriabandini <100641226+hectoriabandini@users.noreply.github.com>
2023-03-19 11:41:11 +0100

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,7 +7,7 @@
 
 /**
  * main - Prints the last digit of a randomly generated number
- * 	and whether it is greater than 5, less than 6, or 0.
+ *	and whether it is greater than 5, less than 6, or 0.
  * Return: Always 0.
  */
 
