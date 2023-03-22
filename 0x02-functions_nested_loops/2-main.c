919e400a33405a6ef87fdfe3706bf5ccad6f47e9
hectoriabandini <abuhmoseji@gmail.com>
2023-03-22 00:57:16 +0000

    2-print corrected

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -3,7 +3,7 @@
 /**
  * main - check the code for Holberton School students.
  *
- *Return: Always 0.
+ * Return: Always 0.
 */
 
 int main(void)
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,23 @@
 #include "main.h"
 
 /**
- * print_alphabet_10-prints_x10 - make alphabet x10 times
- *
- * Return: Void
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
  */
 
 void print_alphabet_x10(void)
 
 {
-	char ch;
-	int i;
+	char c;
+	int i = 0;
 
-	i = 0;
-	while (i < 10)
+	while (i <= 9)
 	{
-	ch = 'a';
-	while (ch <= 'z')
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(ch);
-		ch++;
+	_putchar(c);
 	}
-	_putchar(ch);
+	_putchar('\n');
 	i++;
 	}
 }
