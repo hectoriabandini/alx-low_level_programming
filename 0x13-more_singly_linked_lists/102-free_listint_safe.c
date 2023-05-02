8c98ab831b80f563b5dac5e2e646a965f9205699
hectoriabandini <abuhmoseji@gmail.com>
2023-05-02 00:55:27 +0000

    102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *temp = NULL;
+
+	while (*h != NULL) {
+		if (*h <= (*h)->next) {  // check for cycle
+			temp = (*h)->next;
+			(*h)->next = NULL;
+			free(*h);
+			*h = temp;
+			count++;
+		} else {
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			count++;
+		}
+	}
+
+	return count;
+}
