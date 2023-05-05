08d9891a5979ce42bd0a46aea829a5ebb213a4be
hectoriabandini <abuhmoseji@gmail.com>
2023-05-05 20:39:47 +0000

    task 3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,5 +9,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return ((n >> index) & 1);
+	int bit_value;
+
+	if (index > 63)
+		return (-1);
+
+	bit_value = (n >> index) & 1;
+
+	return (bit_value);
 }
