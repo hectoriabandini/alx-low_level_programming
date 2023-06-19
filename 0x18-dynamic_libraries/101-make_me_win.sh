f3b011fc5c3f1f70570c3058224d3b602aeeb587
hectoriabandini <abuhmoseji@gmail.com>
2023-06-19 14:57:09 -0700

    pool

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-select_random_numbers() { local quantity=$1 pool=("${@:2}"); shuffled_pool=($(shuf -e "${pool[@]}")); selected_numbers=("${shuffled_pool[@]:0:quantity}"); echo "${selected_numbers[@]}"; }
-pool_1_to_75=({1..75}); pool_1_to_15=({1..15}); selected_numbers_1_to_75=$(select_random_numbers 5 "${pool_1_to_75[@]}"); selected_number_1_to_15=$(select_random_numbers 1 "${pool_1_to_15[@]}"); echo "Selected numbers (1 to 75): ${selected_numbers_1_to_75[*]}"; echo "Selected number (1 to 15): $selected_number_1_to_15"
+ls -al
+md5sum giga_millions_program | awk '{print $1}' | grep -q c4ca4238a0b923820dcc509a6f75849b && gm; exit
